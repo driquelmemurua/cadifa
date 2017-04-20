7b3179f01c037d47129b59a392bb063b042bb408
Diego Riquelme Murua <driquelmemurua@gmail.com>
2017-04-19 23:13:05 -0300

    modificado controlador RESTful

--- blog/app/Http/Controllers/EntradasController.php
+++ blog/app/Http/Controllers/EntradasController.php
@@ -8,89 +8,47 @@ use App\Entradas;
 
 class EntradasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return Entradas::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function show($id)
     {
-        //
+        return Entradas::find($id);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $entrada = new Entrada;
 
+        $entrada->id_administrador = request('id_administrador');
+
         $entrada->titulo = request('titulo');
 
-        $entrada->contenido = request('contenido');
-
-        $entrada->preview = substr(request('contenido'), 0, 50);
+        $entrada->ruta = request('ruta');
 
         $entrada->save();
 
+        return response(201);
+    }
+
+    public function update($id, $titulo = null)
+    {
+        if($titulo)
+        {
+            $entrada = Entradas::find($id);
+            $entrada->titulo = $titulo;
+            return response(200);
+        }
+        return response(304);
+    }
+
+    public function delete($id)
+    {
+        $entrada = Entradas::find($id);
+        $entrada->delete();
         return response(200);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Entradas $entrada)
-    {
-        return $entrada;
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
