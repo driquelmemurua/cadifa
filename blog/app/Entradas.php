9bd41173d1feb41872c0ed27592bfad74c6284b5
Diego Riquelme Murua <driquelmemurua@gmail.com>
2017-04-19 23:10:24 -0300

    creada relacion con comentarios

--- blog/app/Entradas.php
+++ blog/app/Entradas.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entradas extends Model
 {
-    //
+   	public function comentarios()
+    {
+        return $this->hasMany('App\Comentarios');
+    }
 }
