26bdeb44910b43484a3e38d714a67d513efa2520
vagrant <vagrant@homestead>
2017-05-20 07:56:52 +0000

    rutas api eliminadas

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -12,14 +12,3 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-/* Entradas */
-Route::get('/api/entradas', 'EntradasController@index');
-Route::get('/api/entradas/{id}', 'EntradasController@show');
-Route::post('/api/entradas', 'EntradasController@store');
-Route::put('/api/entradas/{id}/{titulo?}', 'EntradasController@update');
-Route::delete('/api/entradas/{id}', 'EntradasController@delete');
-
-/* Comentarios */
-Route::get('/api/entradas/comentarios', 'ComentariosController@index');
-Route::get('/api/entradas/{id}/comentarios', 'ComentariosController@show');
