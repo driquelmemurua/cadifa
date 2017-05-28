7503254b2cd71bb5162da61da38733464bc59d37
Diego Riquelme Murua <driquelmemurua@gmail.com>
2017-05-28 14:04:08 -0400

    servicio que recoge 5 historias segun pagina

--- blog/app/EntriesService.php
+++ blog/app/EntriesService.php
@@ -10,4 +10,25 @@ class EntriesService
     {
         
     }
+
+    public function getStories($page)
+    {
+    	$stories = [
+    		0 => NULL,
+    		1 => NULL,
+    		2 => NULL,
+    		3 => NULL,
+    		4 => NULL,
+    	];
+        $result = Entry::with('stories', 'likes', 'comments')
+        				->orderBy('id', 'desc')
+        				->take(5 * $page)
+        				->get();
+        $end = count($result)-1;
+        for($i = $end, $j = 0;($i > 0 && $j < 5); $i--, $j++){
+        	$stories [$j] = $result[$i];
+        }
+
+        return $stories;
+    }
 }
--- blog/app/Http/Controllers/StoriesController.php
+++ blog/app/Http/Controllers/StoriesController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 
 class StoriesController extends Controller
 {
-    public function index($page = null)
+    public function index()
     {
+    	$page = Input::get('page', 1);
     	$entries = $service->sortOldEntries();
-    	$stories = $service->getEntries($page);
+    	$stories = $service->getStories($page);
         return view('home');
     }
 }
