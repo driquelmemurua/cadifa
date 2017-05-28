93cec966b5ab9fc3d582ff582f999a9917621e94
Diego Riquelme Murua <driquelmemurua@gmail.com>
2017-05-28 14:54:55 -0400

    bugfix

--- blog/app/Http/Controllers/StoriesController.php
+++ blog/app/Http/Controllers/StoriesController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\EntriesService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class StoriesController extends Controller
 {
-    public function index()
+    public function index(EntriesService $service)
     {
     	$page = Input::get('page', 1);
-    	$entries = $service->sortOldEntries();
+    	//$entries = $service->sortOldEntries();
     	$stories = $service->getStories($page);
         return view('home', $entries, $stories);
     }
