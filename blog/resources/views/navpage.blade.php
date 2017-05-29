99231d295135e0edb7ad73a3b777dc3f843fe245
Diego Riquelme Murua <driquelmemurua@gmail.com>
2017-05-29 19:09:17 -0400

    tutorial navpage

--- blog/resources/views/navpage.blade.php
+++ blog/resources/views/navpage.blade.php
@@ -1,3 +1,7 @@
+{{"Navegacion de paginas (blog/resources/views/navpage.blade.php)"}} <br>
+{{"Si la pag es mayor a 1 imprime flecha izquierda (regresa 1 pagina)"}}<br>
+{{"Si la pagina es menor a la pagina final imprime flecha derecha (avanza una pagina)"}}<br>
+{{"En el centro de imprime la pagina actual"}}
 @if($page > 1)
 <a href="{{ route($type) }}{{ '?page=' }}{{ $page-1 }}"><img src="http://icons.iconarchive.com/icons/icons8/ios7/128/Arrows-Left-2-icon.png"></a>
 @endif
