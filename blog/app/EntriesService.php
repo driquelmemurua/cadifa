904bc6974939f66e8916fa990b2735ca28b786a1
Diego Riquelme Murúa <driquelmemurua@gmail.com>
2017-05-29 08:54:20 -0400

    Update EntriesService.php

--- blog/app/EntriesService.php
+++ blog/app/EntriesService.php
@@ -27,7 +27,7 @@ class EntriesService
         $result = Entry::select(DB::raw('entries.id as id, comments.id as comment_id, entries.title as entries_title, entries.created_at as creation_date, comments.content as comment_content, stories.content as stories_content, count(likes.entry_id) as likes_count'))
 			->join('stories', 'entries.id', '=', 'stories.entry_id')
 	    	->leftJoin('likes', 'entries.id', '=', 'likes.entry_id') 
-	    	->leftJoin('comments', 'entries.id' ,'=', 'comments.entry_id')
+	    	//->leftJoin('comments', 'entries.id' ,'=', 'comments.entry_id')
 			->orderBy('entries.created_at', 'desc')
 	    	->skip($page-1 * $quantity)
 			->take($quantity)
