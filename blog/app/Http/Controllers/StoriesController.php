7e9d671d1f4a801749a1ef6240defd5f326766f1
Fabián Miranda Muñoz <fabian@THINKPAD.Fabian>
2017-05-29 01:00:40 -0400

    Cambio en storiescontroller

--- blog/app/Http/Controllers/StoriesController.php
+++ blog/app/Http/Controllers/StoriesController.php
@@ -13,7 +13,8 @@ class StoriesController extends Controller
     {
     	$page = Input::get('page', 1);
     	/*$entries = $service->sortOldEntries();*/
-    	$stories = $service->getStories($page);
+    	$quantity = 5; /*A MODO DE PRUEBA, EXIGIMOS SI O SI SOLO 5 stories*/
+    	$stories = $service->getStories($page, $quantity);
         return /*view('stories', /*$entries,/ $stories)*/View::make('stories')->with('stories', $stories); /*Imprime el arreglo de stories del sistema */
     }
 }
