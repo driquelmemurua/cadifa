912b78625a8a23e314d669c6887b1c838e2e885a
corals <dember_elcarmen@hotmail.com>
2017-05-29 06:45:30 -0400

    vistas de historias

--- blog/resources/views/stories.blade.php
+++ blog/resources/views/stories.blade.php
@@ -15,13 +15,42 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
+                <div class="panel-heading">Entradas</div>
+
                     @foreach ($stories as $story)
-                    {{$story}}
-                    @endforeach
+
+                    <div class="panel-heading">
+                            <h1>{{$story->entries_title}}</h1>
+
+                    <div class="panel-body">
+
+                        <div class="row">
+                            
+                            <div class="col-md-8 col-md-offset-0.3">
+
+                                <h4>{{$story->creation_date}}</h4>
+                            
+                            </div>
+                        
+                        </div>
+
+                        <div class="row">
+                            
+                            <div class="col-md-8">
+
+                                <div class="entry">
+
+                                    <h3>{{$story->stories_content}}</h3>
+                                
+                                </div>
+
+                            </div>    
+                        
+                        </div>
+                    </div>
                 </div>
+                @endforeach            
             </div>
         </div>
     </div>
