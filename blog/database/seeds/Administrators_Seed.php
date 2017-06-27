2aab584f7905e29cbd330064f8c7b960194a3949
Fabián Miranda Muñoz <fabian@THINKPAD.Fabian>
2017-06-27 18:07:50 -0400

    Agregado Seeder de 2 administrators

---
+++ blog/database/seeds/Administrators_Seed.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Administrators_Seed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+	public function run(){
+	    $faker = Faker\Factory::create();
+	    $min_date = strtotime("10 March 2017");
+    	$max_date = strtotime("29 June 2017");
+
+	    for( $i = 0; $i < 2; $i++ ) {
+	    	$rand_created_at = rand($min_date, $max_date);
+    		$rand_updated_at = rand($rand_created_at, $max_date);
+
+	    	$created_at = date("Y-m-d H:i:s", $rand_created_at);
+	    	$updated_at = date("Y-m-d H:i:s", $rand_updated_at);
+
+	        DB::table('administrators')->insert([
+            'id' => $i,
+            'name' => $faker->name,
+            'email' => $faker->email,
+            'password' => bcrypt('secret'),
+            'created_at' => $created_at,
+            'updated_at' => $updated_at /*Debe terminar sin coma*/
+            ]);
+        }
+    }
+}
